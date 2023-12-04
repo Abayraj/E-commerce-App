f60530083a2c75e7ceee0b745998c16874f37a02
Abayraj <abayunni5@gmail.com>
2023-12-04 23:17:35 +0530

    navbar  completed slider section on going

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
+import { Badge } from "@mui/material";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+
+const Container = styled.div`
+    height: 60px;
+`;
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+`;
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`;
+
+const Center = styled.div`
+    flex: 1;
+    text-align: center;
+`;
+
+const Right = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
+`;
+
+const MeanuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left:25px
+
+`;
+const SearchContainer = styled.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 2px;
+`;
+const Input = styled.input`
+    border: none;
+`;
+const Logo = styled.h1`
+    font-weight: bold;
+`;
+
+const Navbar = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Left>
+                    <Language>En</Language>
+                    <SearchContainer>
+                        <Input />
+                        <SearchIcon style={{color:"gray",fontSize:18}} />
+                    </SearchContainer>
+                </Left>
+                <Center>
+                    <Logo>MAMA</Logo>
+                </Center>
+                <Right>
+                    <MeanuItem>REGISTER</MeanuItem>
+                    <MeanuItem>SIGN IN</MeanuItem>
+                    <MeanuItem>
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartOutlinedIcon />
+                        </Badge>
+                    </MeanuItem>
+                </Right>
+            </Wrapper>
+        </Container>
+    );
+};
+
+export default Navbar;
---
+++ Frontend/src/components/Slider.jsx
@@ -0,0 +1,119 @@
+import styled from "styled-components";
+import ArrowLeftOutlinedIcon from "@mui/icons-material/ArrowLeftOutlined";
+import ArrowRightOutlinedIcon from "@mui/icons-material/ArrowRightOutlined";
+import { useState } from "react";
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    position: relative;
+    overflow:hidden;
+`;
+
+const Arrow = styled.div`
+    width: 50px;
+    height: 50px;
+    background-color: white;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    left: ${(props) => props.direction === "left" && "10px"};
+    right: ${(props) => props.direction === "right" && "10px"};
+    cursor: pointer;
+    opacity: 0.8;
+    z-index:2;
+`;
+
+const Wrapper = styled.div`
+    height: 100%;
+    display:flex;
+    transform:translateX(0vw);
+`;
+const Slide = styled.div`
+    width: 100vw;
+    height:100vh;
+    display: flex;
+    align-items: center;
+`;
+const ImageContainer = styled.div`
+height:100%;
+
+    flex: 1;
+`;
+const Image = styled.img`
+height:80%;
+`;
+
+const InfoContainer = styled.div`
+    flex: 1;
+`;
+const Title = styled.h1`
+    font-size: 70px;
+`;
+const Description = styled.p`
+margin:50px 0px;
+font-size:20px;
+font-weight: 600;
+letter-spacing:3px
+`;
+const Button = styled.button`
+padding:10px;
+font-size:20px;
+background-color:transparent;
+cursor: pointer;
+`;
+
+
+const Slider = () => {
+  const [slideIndex,setSlideIndex] = useState(0);
+  const  handleClick = (direction)=>{
+
+  };
+    return (
+        <Container>
+            {/* prop direction */}
+            <Arrow direction="left" onClick={()=>handleClick("left")}>
+                <ArrowLeftOutlinedIcon />
+            </Arrow>
+            <Wrapper>
+                <Slide>
+                    <ImageContainer>
+                        <Image src="../../public/pexels-koolshooters-6976001.jpg" />
+                    </ImageContainer>
+                    <InfoContainer>
+                        <Title> SUMMER SALE</Title>
+                        <Description>
+                            DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW
+                            ARRIVALS.
+                        </Description>
+                        <Button>SHOP NOW</Button>
+                    </InfoContainer>
+                </Slide>
+                <Slide>
+                    <ImageContainer>
+                        <Image src="../../public/pexels-koolshooters-6976001.jpg" />
+                    </ImageContainer>
+                    <InfoContainer>
+                        <Title> SUMMER SALE</Title>
+                        <Description>
+                            DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW
+                            ARRIVALS.
+                        </Description>
+                        <Button>SHOP NOW</Button>
+                    </InfoContainer>
+                </Slide>
+            </Wrapper>
+            <Arrow direction="right" onClick={()=>handleClick("right")}>
+                <ArrowRightOutlinedIcon />
+            </Arrow>
+        </Container>
+    );
+};
+
+export default Slider;
