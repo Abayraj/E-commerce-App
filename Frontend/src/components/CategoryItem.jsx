ac2bc91adcdeddf039292202b95b5d7af10a4747
Abayraj <abayunni5@gmail.com>
2023-12-06 12:55:17 +0530

    Category and product list done

---
+++ Frontend/src/components/CategoryItem.jsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    flex: 1;
+    margin:3px
+    height:70vh;
+    position:relative;
+`;
+const Image = styled.img`
+    width: 100%;
+    height:100%;
+    object-fit:cover;
+`;
+const Title = styled.h1`
+color:white;
+margin:20px;
+`;
+const Info = styled.div`
+position:absolute;
+width:100%;
+height:100%;
+top:0;
+left:0;
+display:flex;
+align-items:center;
+justify-content:center;
+flex-direction:column;
+`;
+const Button = styled.button`
+padding: 10px;
+font-size: 20px;
+background-color: white;
+cursor: pointer;
+color:gray;
+border: none;
+
+
+`;
+
+const CategoryItem = ({ items }) => {
+    return (
+        <Container>
+            <Image src={items.imageUrl} />
+            <Info>
+                <Title>{items.title}</Title>
+                <Button>SHOP NOW</Button>
+            </Info>
+        </Container>
+    );
+};
+
+export default CategoryItem;
--- Frontend/src/data.js
+++ Frontend/src/data.js
@@ -19,4 +19,69 @@ export const sliderItems = [
     },
   ];
 
+ export const categories = [
+    {
+      id: 1,
+      imageUrl:'../public/pexels-koolshooters-6976001.jpg',
+      title: 'LIGHT SHIRT',
+    },
+    {
+      id: 2,
+      imageUrl:'../public/pexels-ira-dulger-1452128.jpg',
+      title: 'FORMAL SHIRT',
+    },
+    {
+      id: 3,
+      imageUrl:'../public/pexels-godisable-jacob-794062.jpg',
+      title: 'CASUAL PANTS',
+    },
+
+  ];
+  
+
+  export const PopularProducts = [
+    {
+      id:1,
+      img:'../public/pexels-godisable-jacob-794062.jpg',
+    },
+    {
+      id:2,
+      img:'../public/pexels-ira-dulger-1452128.jpg',
+    },
+    {
+      id:3,
+      img:'../public/pexels-koolshooters-6976001.jpg',
+    },
+    {
+      id:4,
+      img:'../public/pexels-godisable-jacob-794062.jpg',
+    },
+    {
+      id:5,
+      img:'../public/pexels-ira-dulger-1452128.jpg',
+    },
+    {
+      id:6,
+      img:'../public/pexels-koolshooters-6976001.jpg',
+    },
+    {
+      id:7,
+      img:'../public/pexels-godisable-jacob-794062.jpg',
+    },
+    {
+      id:8,
+      img:'../public/pexels-ira-dulger-1452128.jpg',
+    },
+    {
+      id:9,
+      img:'../public/pexels-koolshooters-6976001.jpg',
+    },
+    {
+      id:10,
+      img:'../public/pexels-koolshooters-6976001.jpg',
+    }
+  
+  
+  ]
+
   
