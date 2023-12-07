d5b6272b2727a85f5e1ba4f9143ea4a9ab4803bc
Abayraj <abayunni5@gmail.com>
2023-12-07 12:51:12 +0530

    Product list page filter sort design done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
+import ProductList from './pages/ProductList'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <Home/>
-        </div>
+        {/* <Home/> */}
+        <ProductList/>
+       
         
     </>
   )
