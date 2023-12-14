0ebcfe084b05b021817d408e14d02a13bff59892
Abayraj <abayunni5@gmail.com>
2023-12-14 12:24:39 +0530

    mongo db connection done

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const PORT = 5000;
+mongoose.connect(process.env.MOGO_CONNECTION_URL)
+  .then(() => {
+    console.log("DB Connection successful")
 
-app.listen(PORT,()=>{
-    console.log(`server is on ${PORT}`)
-
-});
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err)=>{
+    console.log(err);
+})
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
