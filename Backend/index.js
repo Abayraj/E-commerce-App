c77fd8f674d293267a2fc135a8679c2f779daf0d
Abayraj <abayunni5@gmail.com>
2023-12-14 16:31:21 +0530

    Models completed

--- Backend/index.js
+++ Backend/index.js
@@ -2,19 +2,25 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require('./routes/user')
+
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MOGO_CONNECTION_URL)
-  .then(() => {
-    console.log("DB Connection successful")
-
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
+    .then(() => {
+        console.log("DB Connection successful")
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
     });
-  })
-  .catch((err)=>{
-    console.log(err);
-})
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+
+
+app.use("/api/users", userRoute);
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+
+
+
